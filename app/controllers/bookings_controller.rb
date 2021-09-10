bed70303156c7973d89fca4234674eaf11bd41bb
Haess Romani <hromaza@gmail.com>
2021-09-10 16:01:52 -0500

    fix: change to .last to get last item in stops array

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,7 +5,7 @@ class BookingsController < ApplicationController
 
   # GET /bookings or /bookings.json
   def index
-    @bookings = Booking.all
+    # @bookings = Booking.all
     begin
       @response = Booking.get_bookings
     rescue RestClient::Exception => exception
